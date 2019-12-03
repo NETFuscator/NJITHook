be008e7712200e55ae8386cb9006810224bf6455
Jon Nowak <notsquirr3l@protonmail.com>
2019-12-03 19:02:33 +1300

    Add files

---
+++ NJITHook/JITHook.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+namespace NJITHook {
+    public class JITHook {
+        public unsafe JITHook(Delegate hook, Version version) {
+            VMT vmt;
+
+            RuntimeHelpers.PrepareDelegate(hook);
+
+            foreach (var method in typeof(JITHook).GetMethods())
+                RuntimeHelpers.PrepareMethod(method.MethodHandle);
+
+            switch (version) {
+                case Version.CORJit:
+                    vmt = new VMT(GetJit_CorJit());
+                    break;
+
+                case Version.CLRJit:
+                    vmt = new VMT(GetJit_ClrJit());
+                    break;
+
+                default:
+                    throw new NotImplementedException();
+            }
+
+            Delegate compileMethodDelegate = null;
+
+            if (IntPtr.Size == 4) {
+                vmt.Hook(0, hook, ref compileMethodDelegate, typeof(Data.CompileMethodDel32));
+                this._compileMethod32 = (Data.CompileMethodDel32)compileMethodDelegate;
+            }
+            else {
+                vmt.Hook(0, hook, ref compileMethodDelegate, typeof(Data.CompileMethodDel64));
+                this._compileMethod64 = (Data.CompileMethodDel64)compileMethodDelegate;
+            }
+
+            this._vmt = vmt;
+        }
+
+        public enum Version {
+            CORJit,
+            CLRJit
+        }
+
+        public unsafe int CompileMethod(IntPtr thisPtr, IntPtr corJitInfo, Data.CorMethodInfo32* methodInfo, Data.CorJitFlag flags, IntPtr nativeEntry, IntPtr nativeSizeOfCode) {
+            return this._compileMethod32(thisPtr, corJitInfo, methodInfo, flags, nativeEntry, nativeSizeOfCode);
+        }
+
+        public unsafe int CompileMethod(IntPtr thisPtr, IntPtr corJitInfo, Data.CorMethodInfo64* methodInfo, Data.CorJitFlag flags, IntPtr nativeEntry, IntPtr nativeSizeOfCode) {
+            return this._compileMethod64(thisPtr, corJitInfo, methodInfo, flags, nativeEntry, nativeSizeOfCode);
+        }
+
+        [DllImport("corjit.dll", EntryPoint = "getJit")]
+        private static extern IntPtr GetJit_CorJit();
+
+        [DllImport("clrjit.dll", EntryPoint = "getJit")]
+        private static extern IntPtr GetJit_ClrJit();
+
+        private Data.CompileMethodDel32 _compileMethod32;
+        private Data.CompileMethodDel64 _compileMethod64;
+
+        private VMT _vmt;
+    }
+}
---
+++ NJITHook/VMT.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace NJITHook {
+    internal class VMT {
+        internal unsafe VMT(IntPtr obj) {
+            this._cls = (void***)obj;
+
+            this._old_table = *this._cls;
+
+            uint size = 0;
+
+            while (this._old_table[size++] != null)
+                ;
+
+            this._new_table = (void**)Marshal.AllocHGlobal((int)(size * IntPtr.Size));
+
+            for (var i = 0; i < size; i++)
+                this._new_table[i] = this._old_table[i];
+
+            *this._cls = this._new_table;
+        }
+        
+        unsafe ~VMT() {
+            *this._cls = this._old_table;
+            Marshal.FreeHGlobal((IntPtr)this._new_table);
+        }
+
+        internal unsafe void Hook(uint index, Delegate hook, ref Delegate original, Type delegateType) {
+            original = Marshal.GetDelegateForFunctionPointer((IntPtr)this._old_table[index], delegateType);
+            
+            this._new_table[index] = (void*)Marshal.GetFunctionPointerForDelegate(hook);
+        }
+
+        private unsafe void*** _cls;
+        private unsafe void** _old_table;
+        private unsafe void** _new_table;
+    }
+}
